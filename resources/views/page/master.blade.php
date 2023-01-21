8c25db73b2f9381e85b9c335bba78d6d1766713e
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2023-01-21 17:42:30 +0500

    edit login style

--- resources/views/page/master.blade.php
+++ resources/views/page/master.blade.php
@@ -17,7 +17,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 
     <!-- Font Awesome -->
-    <link href="{{asset('assets/cdndan/all/min/css')}}" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
     <link href="{{asset('assets/lib/animate/animate.min.css')}}" rel="stylesheet">
@@ -254,8 +255,8 @@
 
 
 <!-- JavaScript Libraries -->
-<script src="{{asset('assets/cdndan/jquery34.min.js')}}"></script>
-<script src="{{asset('assets/cdndan/bootstrap.bundle.min.js')}}"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
 <script src="{{asset('assets/lib/easing/easing.min.js')}}"></script>
 <script src="{{asset('assets/lib/owlcarousel/owl.carousel.min.js')}}"></script>
 
