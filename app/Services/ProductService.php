5933878e102aeeba7ab9e82e1b7714c6d2b4bdd2
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2023-02-11 23:41:10 +0500

    start working with services

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -8,7 +8,7 @@ class ProductService
     public static function create($data)
     {
         $product = new Product();
-        //:-)
+        //:)
         $product->name = $data['name'];
         $product->price = $data['price'];
         $product->save();
